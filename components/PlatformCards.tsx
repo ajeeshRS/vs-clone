76cca99b2ee15fe76d6e01f9457d98e7a308e679
ajeeshRS <ajeeshrs79@gmail.com>
2024-09-10 22:52:14 +0530

    Fix card height issue

--- components/PlatformCards.tsx
+++ components/PlatformCards.tsx
@@ -8,7 +8,7 @@ export default function PlatformCards() {
       {platformCardsData.map((platform, i) => (
         <div
           key={i}
-          className="sm:h-[55vh] h-[40vh] sm:w-2/6 w-6/6  hover:shadow-md hover:shadow-violet-800 transition-shadow duration-200 ease-in-out  bg-[#0F0922] bg-opacity-40 flex flex-col sm:mx-5 mx-2 rounded-xl text-white items-start justify-between sm:p-10 p-3 group sm:my-0 my-3"
+          className="sm:h-[55vh] h-fit sm:w-2/6 w-6/6  hover:shadow-md hover:shadow-violet-800 transition-shadow duration-200 ease-in-out  bg-[#0F0922] bg-opacity-40 flex flex-col sm:mx-5 mx-2 rounded-xl text-white items-start justify-between sm:p-10 p-3 group sm:my-0 my-3"
         >
           <p className="sm:text-2xl text-xl bg-gradient-to-b bg-clip-text text-transparent from-[#EAE3FF] to-[#BBA1FF] inline-block font-medium">
             {platform.title}
